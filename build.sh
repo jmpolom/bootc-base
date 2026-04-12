ac090ef9033c61e075f931f1eeaa62092bc597da
Jon Polom <jon@subly.me>
2026-04-11 23:07:38 -0400

    maybe a basic build script

--- build.sh
+++ build.sh
@@ -1,3 +1,29 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-podman build --security-opt=label=disable --cap-add=all --device /dev/fuse -t fedora-bootc-minimal:latest -f Containerfile
+name="fedora-bootc-minimal"
+release="43"
+tag="main"
+
+while getopts ":n:r:t:" opt; do
+    case ${opt} in
+        n)
+            name="$OPTARG"
+            ;;
+        r)
+            release="$OPTARG"
+            ;;
+        t)
+            tag="$OPTARG"
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 100
+            ;;
+        :)
+            echo "Missing argument: -$OPTARG" >&2
+            exit 120
+            ;;
+    esac
+done
+
+podman build --security-opt=label=disable --cap-add=all --device /dev/fuse --build-arg RELEASE="${release}" -t "${name}":"${release}"-"${tag}" -f Containerfile
