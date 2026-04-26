e5d96f5efeb3d0eb06dd1cc8bca44301db665df7
Jon Polom <jon@subly.me>
2026-04-25 22:20:28 -0400

    updated build script

--- build.sh
+++ build.sh
@@ -1,29 +1,58 @@
 #!/usr/bin/env bash
 
-name="fedora-bootc-minimal"
-release="43"
-tag="main"
+set -xeu
 
-while getopts ":n:r:t:" opt; do
+# defaults
+name="fedora-bootc-minimal"
+rel="43"
+ts="main"
+
+while getopts ":d:n:r:s:" opt; do
     case ${opt} in
+        d)
+            registry="$OPTARG"
+            ;;
         n)
             name="$OPTARG"
             ;;
         r)
-            release="$OPTARG"
+            rel="$OPTARG"
             ;;
-        t)
-            tag="$OPTARG"
+        s)
+            # tag suffixes
+            tss+=("$OPTARG")
             ;;
         \?)
             echo "Invalid option: -$OPTARG" >&2
-            exit 100
+            exit 1
             ;;
         :)
             echo "Missing argument: -$OPTARG" >&2
-            exit 120
+            exit 2
             ;;
     esac
 done
 
-podman build --security-opt=label=disable --cap-add=all --device /dev/fuse --build-arg RELEASE="${release}" -t "${name}":"${release}"-"${tag}" -f Containerfile
+# construct image names with suffixed tags
+if [ -v tss ]; then
+    for t in "${tss[@]}"; do
+        tagged_names+=("${name}:${rel}-${t}")
+    done
+else
+    tagged_names+=("${name}:${rel}-${ts}")
+fi
+
+# construct array of tag opts for podman build
+for n in "${tagged_names[@]}"; do
+    tag_opts+=("-t" "${n}")
+done
+
+# build the container
+podman build --security-opt=label=disable --cap-add=all --device /dev/fuse --build-arg RELEASE="${rel}" "${tag_opts[@]}" -f Containerfile .
+
+# push if a registry was specified
+if [ -v registry ]; then
+    for n in "${tagged_names[@]}"; do
+        podman push "${n}" "${registry}/${n}"
+    done
+fi
